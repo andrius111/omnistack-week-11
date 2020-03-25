b9e4ed8a324ec7dcd01f73646bbd4782d03a152a
Andrius Martini <andrius.martini1994@gmail.com>
2020-03-25 00:39:35 -0300

    Backend application

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,18 @@
+const connection = require('../database/connection')
+
+
+module.exports = {
+  async index(request, response) {
+    const ong_id = request.headers.authorization
+
+    if (!ong_id) {
+      return response.status(401).json({ error: 'Unauthorized.' })
+    }
+
+    const incidents = await connection('incidents')
+                            .select('*')
+                            .where('ong_id', ong_id)
+
+    return response.json(incidents)
+  }
+}
